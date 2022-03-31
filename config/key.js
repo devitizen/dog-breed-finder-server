0d9cf4e0b97b91072e7d272435f8cc39e079970c
Jonghwan Lee <devitizen@gmail.com>
2022-03-31 10:05:13 -0400

    first commit

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : "mongodb+srv://john1017:mega1104@cluster0.3h1xl.mongodb.net?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const cors = require("cors");
+app.use(cors());
+
+const { MongoClient } = require("mongodb");
+const key = require("./config/key");
+const client = new MongoClient(key.mongoURI);
+
+async function run(name) {
+    try {
+        await client.connect();
+        const database = client.db("dogBreedFinder");
+        const breeds = database.collection("breeds");
+        return await breeds.findOne({ name: name });
+    } finally {
+        await client.close();
+    }
+}
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World.");
+});
+
+
+app.get("/api/breeds", (req, res) => {
+    const name = req.query.name;
+    run(name)
+        .then((breed) => {return res.status(200).send(breed)})
+        .catch(console.dir);
+});
+
+
+// listening
+app.listen(port, () => {
+    console.log(`Dog Breed Finder DB listening on port ${port}`);
+});
